2007cf97c73bfb304d75d209291373269e0a57cb
Jayphen <jayphen@hyperlab.se>
2020-02-28 17:15:26 +0100

    Initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint"
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    project: "./tsconfig.json"
+  },
+  env: {
+    node: true
+  }
+};
